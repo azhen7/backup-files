6ae2e1afe76a165b97f5802e9a455d003d895570
adam zheng <adam.ray.zheng@gmail.com>
2021-03-25 14:04:16 -0400

    check in log.h

---
+++ log.h
@@ -0,0 +1,38 @@
+#include <math.h>
+
+int main(void);
+
+double log3(float number)
+{
+    return log10(number) / log10(3);
+}
+
+double log4(float number)
+{
+    return log10(number) / log10(4);
+}
+
+double log5(float number)
+{
+    return log10(number) / log10(5);
+}
+
+double log6(float number)
+{
+    return log10(number) / log10(6);
+}
+
+double log7(float number)
+{
+    return log10(number) / log10(7);
+}
+
+double log8(float number)
+{
+    return log10(number) / log10(8);
+}
+
+double log9(float number)
+{
+    return log10(number) / log10(9);
+}
