eefbd0dcdb1317c3f9b2fd90154dee2f941a86a4
azhen7 <75392766+azhen7@users.noreply.github.com>
2021-03-29 10:57:21 -0400

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,6 +1,5 @@
 #define _GNU_SOURCE //It's there so the compiler would stop complaining that isascii() does not exist.
 #define ARRAY_SIZE 8
-#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647 //value of pi
 
 #include <stdio.h> //Used for printf()
 #include <cs50.h> //Used for get_string()
@@ -1259,7 +1258,7 @@ long double factorial(float number)
         result *= i;
     }
     if (number != (int) number)
-        result *= sqrt(PI);
+        result *= sqrt(M_PI);
     return result;
 }
 
