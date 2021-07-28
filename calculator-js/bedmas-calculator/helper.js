70c7a7df0d8ee6e6d8615817c7a549a74347dd32
azhen7 <75392766+azhen7@users.noreply.github.com>
2021-07-28 19:19:46 -0400

    Update helper.js

--- calculator-js/bedmas-calculator/helper.js
+++ calculator-js/bedmas-calculator/helper.js
@@ -3,83 +3,63 @@ const PI = 3.141592653;
 const E = 2.718281828;
 const GOLDEN_RT = 1.618033988;
 const SILVER_RT = 2.414213562;
-const APERY_CONST = 1.202056903;
 const SQRT_2 = 1.414213562;
 const SQRT_3 = 1.732050807;
+const mathConsts = [
+  "PI",
+  "E",
+  "GOLDEN_RT",
+  "SILVER_RT",
+  "SQRT_2",
+  "SQRT_3"
+];
 
-/* Check if char is a number */
-function isdigit(c) {
-  if (c == ' ')
-    return 0;
-  if (c >= 48 || c <= 57)
-    return 1;
-  return 0;
-}
-//Returns number of operations
-function numberOfOperations(input) {
-  var times = 0;
-  for (let i = 0; i < input.length; i++) {
-    if (isdigit(input[i])) {
-      continue;
-    }
-    if (validateOperation(input[i]) == 0) {
-      if (validNext(input[i + 1]) == 0) {
-        if (!(input[i] == '-' && isdigit(input[i + 1]) && !isdigit(input[i - 1]))) {
-          times++;
-        }
-      }
-    }
-  }
-  return times;
-}
 //Return numbers present in equation
 function convertFloat(input, startIndex, endIndex) {
-  var lastNum = 0;
-  var total = 0.0;
-  var multNeg = 1;
-  var numNum = 1;
+  let lastNum = 0;
+  let total = 0.0;
+  let multNeg = 1;
+  let numNum = 1;
+  let returnValueOfMathConstant = (input, i) => {
+    const repMathConstsChar = ["Z", "E", "G", "Y", "T", "R"];
+    let index = repMathConstsChar.indexOf(input[i]);
+    return eval(mathConsts[index]);
+  }
   for (let i = startIndex; i < endIndex; i++) {
     if (!isdigit(input[i])) {
       //If input[i] is a decimal point, start adding decimals
-      if (input[i] == '.')
-      {
-        if (!isdigit(input[i + 1]))
-        {
-          if (input[i + 1] == '.' || isalpha(input[i + 1]) || input[i + 1] == ' ') {
-            document.getElementById('result').innerHTML = "Invalid input";
-            return;
-          }
-          else {
-            break;
-          }
+      if (input[i] === ".") {
+        if (!isdigit(input[i + 1])) {
+          return undefined;
         }
         else {
           lastNum = 1;
         }
       }
       //If input[i] is a valid operation, this implies that the first number has terminated
-      else if (validateOperation(input[i]) == 0 || input[i] == ' ') {
+      else if (validateOperation(input[i]) === 0 || input[i] == ' ') {
         break;
       }
       else if (validateConstantChar(input[i])) {
-        total = returnValueOfMathConstant(input, i);
+        if (total === 0)
+            total = 1;
+        total *= returnValueOfMathConstant(input, i);
         break;
       }
-      else if (input[i] == 'm') {
+      else if (input[i] === "m")
         multNeg = -1;
-      }
       else {
-        document.getElementById('result').innerHTML = "Invalid input";
-        return;
+        return undefined;
       }
-    }
+    } 
     else {
-      if (lastNum == 0) {
+      if (lastNum === 0) {
         total += input[i].charCodeAt() - 48;
-        if (isdigit(input[i + 1])) {
-          total *= 10;
+        if (i !== endIndex - 1) {
+          if (isdigit(input[i + 1])) 
+            total *= 10;
         }
-      }
+      } 
       else {
         total += (input[i].charCodeAt() - 48) / Math.pow(10, numNum);
         numNum++;
@@ -90,70 +70,12 @@ function convertFloat(input, startIndex, endIndex) {
   return total;
 }
 
-function returnValueOfMathConstant(input, i) {
-  //Pi
-  if (input[i] == '!') {
-    return getMathConstant(input, i, PI);
-  }
-  //Golden Ratio
-  else if (input[i] == 'G') {
-    return getMathConstant(input, i, GOLDEN_RT);
-  }
-  //Square root of 2
-  else if (input[i] == 'T') {
-    return getMathConstant(input, i, SQRT_2);
-  }
-  //Square root of 3
-  else if (input[i] == 'R') {
-    return getMathConstant(input, i, SQRT_3);
-  }
-  //Euler's number
-  else if (input[i] == 'E') {
-    return getMathConstant(input, i, E);
-  }
-  //Silver Ratio
-  else if (input[i] == 'Y') {
-    return getMathConstant(input, i, SILVER_RT);
-  }
-  //Apery's Constant
-  else if (input[i] == 'A') {
-    return getMathConstant(input, i, APERY_CONST);
-  }
-  return 0;
-}
-
-function getMathConstant(input, index, mathConstant) {
-  var value = 0;
-  var coefForConsts;
-  var lookback = 1;
-  var startIndexCoefficient;
-  if (index > 0 && isdigit(input[index - 1]))
-  {
-      coefForConsts = 0;
-      while ((isdigit(input[index - lookback]) || input[index - lookback] == '.') && index - lookback >= 0)
-      {
-          startIndexCoefficient++;
-          lookback++;
-      }
-      coefForConsts = convertFloat(input, index - startIndexCoefficient, index);
-      value = coefForConsts * mathConstant;
-  }
-  else {
-    value = mathConstant;
-  }
-  return value;
-}
-
-function log_base(a, b) {
-  return Math.log(a) / Math.log(b);
-}
-
-function setNum(operations, index) {
-  if (index == 0) {
+function setNum(operations, index, offset = -1) {
+  if (operations[index + offset] === undefined) {
     return 0;
   }
-  else if (index > 0) {
-    if (operations[index - 1] == '*' || operations[index - 1] == '/') {
+  else {
+    if (operations[index + offset] === "*" || operations[index + offset] === "/" || (operations[index] === "^" && offset >= 0)) {
       return 1;
     }
     else {
@@ -161,49 +83,320 @@ function setNum(operations, index) {
     }
   }
 }
-// Calculates GCD of a and b
-function calculateGCD(a, b) {
-  var temp = 0;
-  if (a < b) {
-    temp = a;
-    a = b;
-    b = temp;
+
+//Get the positions of the numbers
+function getPositionsOfNums(equation, arrSize) {
+  let positions = [];
+  //Get the positions of the numbers
+  for (let i = 0; i < equation.length; i++) {
+    if (isdigit(equation[i])) {
+      if (i > 0) {
+        if (!isdigit(equation[i - 1]) && equation[i - 1] !== 'm' && equation[i - 1] !== '.') {
+          positions.push(i);
+        }
+      }
+      else {
+        positions.push(i);
+      }
+    }
+    else {
+      if (equation[i] === '-' && isdigit(equation[i + 1])) {
+        if (i > 0) {
+          if (validateOperation(equation[i - 1])) {
+            equation = equation.substring(0, i) + 'm' + equation.substring(i + 1);
+            positions.push(i);
+          }
+          else {
+            positions.push(i);
+          }
+        }
+      }
+      else if (validateConstantChar(equation[i])) {
+        if (i > 0) {
+          if (!isdigit(equation[i - 1]) && equation[i - 1] !== '-') {
+            positions.push(i);
+          }
+        }
+        else {
+          positions.push(i);
+        }
+      }
+    }
   }
-  while (b != 0) {
-    temp = b;
-    b = a % b;
-    a = temp;
+  positions.sort((a, b) => a - b); //Sort the positions in case there is some stupid positions confusion
+  return positions;
+}
+
+function getFunctionValue(nums, operations, functions, location, i, counter) {
+  //nth root
+  if (functions[i] === "0") {
+    let negative = 1;
+    if (nums[location] < 0 && nums[location + 1] % 2 !== 0) {
+      nums[location] *= -1;
+      if (nums[location + 1] === Math.floor(nums[location + 1])) {
+        negative = -1;
+      }
+    }
+    if (operations[location - 1] === '^') {
+      nums[location] = Math.pow(nums[location], 1 / nums[location + 1]);
+      nums[location + 1] = setNum(operations, location, 0);
+    }
+    else {
+      nums[location + 1] = Math.pow(nums[location], 1 / nums[location + 1]);
+      nums[location] = setNum(operations, location);
+    }
+    nums[location + 1] *= negative;
   }
-    return a;
-}
-//sinc
-function sinc(x) {
-  if (x == 0) {
-    return 1;
+  //log
+  else if (functions[i] === "1") {
+    if (operations[location - 1] === '^') {
+      nums[location] = log_base(nums[location], nums[location + 1], 1);
+      nums[location + 1] = setNum(operations, location, 0);
+    }
+    else {
+      nums[location + 1] = log_base(nums[location], nums[location + 1]);
+      nums[location] = setNum(operations, location);
+    }
   }
-  return Math.sin(x) / x;
-}
-//secant
-function sec(x) {
-  return 1 / Math.cos(x);
-}
-//cotangent
-function cot(x) {
-  return 1 / Math.tan(x);
-}
-//cosecant
-function cosec(x) {
-  return 1 / Math.sin(x);
-}
-//iterative log
-function iterative_log(a, b) {
-  if (b < 1.444667 || a <= 1) {
-    return -1;
+  //GCD
+  else if (functions[i] === "2") {
+    if (nums[location] !== Math.floor(nums[location]) || nums[location + 1] !== Math.floor(nums[location + 1])) {
+      nums[location + 1] = undefined;
+      return nums;
+    }
+    if (operations[location - 1] === '^') {
+      nums[location] = calculateGCD(nums[location], nums[location + 1], 1);
+      nums[location + 1] = setNum(operations, location, 0);
+    }
+    else {
+      nums[location + 1] = Math.abs(nums[location] * nums[location + 1]) / calculateGCD(nums[location], nums[location + 1]);
+      nums[location] = setNum(operations, location);
+    }
   }
-  var iterative_count = 0;
-  while (a > 1) {
-    iterative_count++;
-    a = log_base(a, b);
+  //LCM
+  else if (functions[i] === "3") {
+    if (nums[location] !== Math.floor(nums[location]) || nums[location + 1] !== Math.floor(nums[location + 1])) {
+      nums[location + 1] = undefined;
+      return nums;
+    }
+    if (operations[location - 1] === '^') {
+      nums[location] = Math.abs(nums[location] * nums[location + 1]) / calculateGCD(nums[location], nums[location + 1]);
+      nums[location + 1] = setNum(operations, location, 0);
+    }
+    else {
+      nums[location + 1] = Math.abs(nums[location] * nums[location + 1]) / calculateGCD(nums[location], nums[location + 1]);
+      nums[location] = setNum(operations, location);
+    }
   }
-  return iterative_count;
+  //sin
+  else if (functions[i] === "4") {
+    if (nums[location] === 3.141592653) {
+      nums[location] = 0;
+    } 
+    else {
+      nums[location] = Math.sin(nums[location]);
+    }
+  }
+  //cos
+  else if (functions[i] === "5") {
+    nums[location] = Math.cos(nums[location]);
+  }
+  //tan
+  else if (functions[i] === "6") {
+    nums[location] = Math.tan(nums[location]);
+  }
+  //cosecant
+  else if (functions[i] === "7") {
+    nums[location] = cosec(nums[location]);
+  }
+  //secant
+  else if (functions[i] === "8") {
+    nums[location] = sec(nums[location]);
+  }
+  //cotangent
+  else if (functions[i] === "9") {
+    nums[location] = cot(nums[location]);
+  }
+  //sinh
+  else if (functions[i] === "a") {
+    nums[location] = Math.sinh(nums[location]);
+  }
+  //cosh
+  else if (functions[i] === "b") {
+    nums[location] = Math.cosh(nums[location]);
+  }
+  //tanh
+  else if (functions[i] === "c") {
+    nums[location] = Math.tanh(nums[location]);
+  }
+  //arcsin
+  else if (functions[i] === "d") {
+    nums[location] = Math.asin(nums[location]);
+  }
+  //arccos
+  else if (functions[i] === "e") {
+    nums[location] = Math.acos(nums[location]);
+  }
+  //arctan
+  else if (functions[i] === "f") {
+    nums[location] = Math.atan(nums[location]);
+  }
+  //arcsinh
+  else if (functions[i] === "g") {
+    nums[location] = Math.asinh(nums[location]);
+  }
+  //arccosh
+  else if (functions[i] === "h") {
+    nums[location] = Math.acosh(nums[location]);
+  }
+  //arctanh
+  else if (functions[i] === "i") {
+    nums[location] = Math.atanh(nums[location]);
+  }
+  //sinc
+  else if (functions[i] === "j") {
+    nums[location] = sinc(nums[location]);
+  }
+  //sqrt
+  else if (functions[i] === "k") {
+    nums[location] = Math.sqrt(nums[location]);
+  }
+  //cbrt
+  else if (functions[i] === "l") {
+    nums[location] = Math.cbrt(nums[location]);
+  }
+  //natural log
+  else if (functions[i] === "o") {
+    nums[location] = Math.log(nums[location]);
+  }
+  //log base 10
+  else if (functions[i] === "p") {
+    nums[location] = Math.log10(nums[location]);
+  }
+  //random number
+  else if (functions[i] === "q") {
+    
+  }
+  //abs
+  else if (functions[i] === "r") {
+    nums[location] = Math.abs(nums[location]);
+  }
+  //ceil
+  else if (functions[i] === "s") {
+    nums[location] = Math.ceil(nums[location]);
+  }
+  //floor
+  else if (functions[i] === "t") {
+    nums[location] = Math.floor(nums[location]);
+  }
+  //round
+  else if (functions[i] === "u") {
+    nums[location] = round(nums[location]);
+  }
+  //iterative log
+  else if (functions[i] === "A") {
+    if (operations[location] === '^') {
+      nums[location] = iterative_log(nums[location], nums[location + 1], 1);
+      nums[location + 1] = setNum(operations, location);
+    }
+    else {
+      nums[location + 1] = iterative_log(nums[location], nums[location + 1]);
+      nums[location] = setNum(operations, location);
+    }
+  }
+  //tetration
+  else if (functions[i] === "B") {
+    if (Math.floor(nums[location + 1]) !== nums[location + 1]) {
+      return undefined;
+    }
+    if (operations[location] === '^') {
+      nums[location] = tetrate(nums[location], nums[location + 1], 1);
+      nums[location + 1] = setNum(operations, location);
+    }
+    else {
+      nums[location + 1] = tetrate(nums[location], nums[location + 1]);
+      nums[location] = setNum(operations, location);
+    }
+  }
+  return nums;
 }
+
+function getLocation(i, times, positions, operations, operationPositions, functionPositions) {
+  for (let j = 0; j < times + 1; j++) {
+    if (functionPositions[i] < positions[j]) {
+      //Check if function takes two inputs (e.g. GCD)
+      if (isFunctionWithTwoArgs(functions[i])) {
+        if (functionPositions[i] > operationPositions[j - 1]) {
+          if (operations[j] === ',') {
+            if ((positions[j] < operationPositions[j] && positions[j + 1] < operationPositions[j + 1]) 
+                || operationPositions[j + 1] === undefined) {
+              return j;
+            }
+            else {
+              return NaN;
+            }
+          }
+          else {
+            return NaN;
+          }
+        }
+        else if (operationPositions[j - 1] === undefined) {
+          if (operations[j] === ',') {
+            return j;
+          }
+          else {
+            return NaN;
+          }
+        }
+      }
+      //If the function takes only one input (e.g. trig functions)
+      else {
+        if (functionPositions[i] > operationPositions[j - 1]) {
+          if (positions[j] < operationPositions[j]) {
+            return j;
+          }
+          else if (operationPositions[j] === undefined || operationPositions[j - 1] === undefined) {
+            return j;
+          }
+          else {
+            return NaN;
+          }
+        }
+        else if (operationPositions[j - 1] === undefined) {
+          return j;
+        }
+        else {
+          return NaN;
+        }
+      }
+    }
+  }
+  return 0;
+}
+let getSubtractVal = (arr, lastChar, str) => {
+  let funcOrConstStr = lastChar;
+  let lookback = 2;
+  while (isalpha(str[str.length - lookback])) {
+    funcOrConstStr += str[str.length - lookback];
+    if (str.length === lookback) {
+      break;
+    }
+    lookback++;
+  }
+  funcOrConstStr = invert_letters(funcOrConstStr);
+  if (arr.includes(funcOrConstStr)) {
+    return funcOrConstStr.length;
+  }
+  else if (["nPr", "nCr"].includes(funcOrConstStr)) {
+    return 3;
+  }
+  return 1;
+};
+//Invert letters in a string
+let invert_letters = (input) => {
+  let invertedStr = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    invertedStr += input[i];
+  }
+  return invertedStr;
+};
