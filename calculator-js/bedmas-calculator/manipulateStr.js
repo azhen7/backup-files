d2fdde1c78d9eb6739bb495dd33624c79fd9f078
azhen7 <75392766+azhen7@users.noreply.github.com>
2021-07-28 19:22:18 -0400

    Update manipulateStr.js

--- calculator-js/bedmas-calculator/manipulateStr.js
+++ calculator-js/bedmas-calculator/manipulateStr.js
@@ -1,79 +1,76 @@
 //Contains all the string-related functions
+function isCharExceptions(c) {
+  if (isdigit(c) || validateConstantChar(c) || c == ')' || c == ' ')
+    return 1;
+  return 0;
+}
 //Check whether c is indicative of function which requires two arguments (e.g. GCD, LCM, log, root, etc.)
 function isFunctionWithTwoArgs(c) {
-  if ((c >= '0'.charCodeAt() && c <= '3'.charCodeAt()) || (c >= 'A'.charCodeAt() && c <= 'B'.charCodeAt)) {
+  if ((c.charCodeAt() >= '0'.charCodeAt() && c.charCodeAt() <= '3'.charCodeAt()) || (c >= 'A'.charCodeAt() && c <= 'B'.charCodeAt)) {
     return 1;
   }
   return 0;
 }
-/* Check whether c is an operation */
+// Check whether c is an operation 
 function validateOperation(c) {
-  if (c == '+' || c == '*' || c == '-' || c == '/' || c == '^' || c == '%' || c == 'e' || c == 'C' || c == 'P' || c == ',' || c == 'x') {
+  if (c == '+' || c == '*' || c == '-' || c == '/' || c == '^' || c == '%' || c == 'e' || c == 'C' || c == 'P' || c == ',' || c == 'x' ||
+      c == '!') {
     return 0;
   }
   return 1;
 }
-/* Check whether char is representative of a math constant (e.g. PI) */
+// Check whether char is representative of a math constant (e.g. PI) 
 function validateConstantChar(c) {
-  if (c == '!' || c == 'T' || c == 'R' || c == 'G' || c == 'E' || c == 'Y' || c == 'A')
+  if (c == 'Z' || c == 'T' || c == 'R' || c == 'G' || c == 'E' || c == 'Y')
       return 1;
   return 0;
 }
-/* Check whether char is first character of a math constant (e.g. PI) */
+// Check whether char is first character of a math constant (e.g. PI) 
 function validateConstantFirstChar(c) {
-  if (c == 'P' || c == 'G' || c == 'S' || c == 'R' || c == 'A' || c == 'C' || c == 'p' || c == 's')
+  if (c == 'P' || c == 'G' || c == 'S' || c == 'R' || c == 'C' || c == 'p' || c == 's')
       return 1;
   return 0;
 }
-/* Validate char */
+// Validate char
 function validNext(c) {
-  if (isdigit(c) || c == 'm' || c == ' ' || c == '-' || validateConstantChar(c))
+  if (isdigit(c) || c == 'm' || c == ' ' || c == '-' || validateConstantChar(c) || validateOperation(c))
       return 0;
   return 1;
 }
-/* Copy string without spaces */
-function copyStrWithoutSpaces(input) {
-  var strWithoutSpaces = '';
-  for (let x = 0; x < input.length; x++) {
-    if (input[x] != ' ') {
-      strWithoutSpaces += input[x];
-    }
-  }
-  return strWithoutSpaces;
+// Check if char is not valid
+function invalidChars(c) {
+  if (c == '<' || c == '>' || c == '?' || c == '@' || c == '#' || c == '$' || c == '&' || c == '{' || c == '}' || c == '[' || c == ']'
+     || c == '"' || c == ';' || c == ':' || c == '`' || c == '_' || c.charCodeAt() > 127)
+    return 1;
+  return 0;
 }
-/* Replaces characters with spaces */
+// Replaces characters with spaces 
 function removeChar(input, index, c) {
-  var changedStr = input.substring(0, index + 1);
+  let changedStr = input.substring(0, index + 1);
   for (let i = 0; i < c; i++) {
-    changedStr[index + 1 + i] += ' ';
+    changedStr += " ";
   }
   changedStr += input.substring(index + c + 1);
   return changedStr;
 }
-/* Validate string */
+// Validate string 
 function isValidInput(input) {
   if (input.length == 0) {
-    return 1;
+    return 0;
   }
-  for (let x = 0; x < input.length; x++) {
-    if (input[x] != ' ') {
-      return 1;
-    }
-  }
-  return 0;
+  return 1;
 }
-/* Set up string */
+// Set up string 
 function setUp(input) {
-  var copy = input;
+  let copy = input;
   for (let i = 0; i < copy.length; i++) {
     if (copy[i] == '+') {
-      if (copy[i + 1] == '-') {
-        copy = copy.substring(0, i) + '-' + copy.substring(i + 2);
+      if (copy[i + 1] == '-' || copy[i + 1] == '+') {
+        copy = copy.substring(0, i) + copy[i + 1] + copy.substring(i + 2);
       }
-      else if (isdigit(copy[i + 1]) || copy[i + 1] == ' ');
+      else if (isCharExceptions(copy[i + 1]));
       else {
-        copy = "NAN";
-        break;
+        return "NAN";
       }
     }
     else if (copy[i] == '-') {
@@ -83,60 +80,51 @@ function setUp(input) {
       else if (copy[i + 1] == '+') {
         copy = copy.substring(0, i) + copy.substring(i + 1);
       }
-      else if (isdigit(copy[i + 1]) || copy[i + 1] == ' ');
+      else if (isCharExceptions(copy[i + 1]));
       else {
-        copy = "NAN";
-        break;
+        return "NAN";
       }
     }
     else if (copy[i] == '*' || copy[i] == 'x') {
       copy = copy.substring(0, i) + '*' + copy.substring(i + 1);
-      if (i > 0)
-      {
-        if (copy[i + 1] == '/')
-        {
-            copy = "NAN";
-            break;
-        }
+      if (i == 0) {
+        return "NAN";
       }
-      else {
-        document.getElementById('result').innerHTML = "Invalid input";
-        return;
+      if (i > 0) {
+        if (copy[i - 1] == '/') {
+          return "NAN";
+        }
       }
       if (copy[i + 1] == '*') {
         copy = copy.substring(0, i) + '^' + copy.substring(i + 2);
       }
-      else if (copy[i + 1] == '+') {
-        copy = copy.substring(0, i) + '-' + copy.substring(i + 1);
-      }
-      else if (isdigit(copy[i + 1]) || copy[i + 1] == '-' || copy[i + 1] == ' ');
-      else
-      {
-        copy = "NAN";
-        break;
+      else if (isCharExceptions(copy[i + 1]) || copy[i + 1] == '-');
+      else {
+        return "NAN";
       }
     }
     else if (copy[i] == '/') {
       if (i == 0) {
-        document.getElementById('result').innerHTML = "Invalid input";
-        return;
+        return "NAN";
       }
-      if (copy[i + 1] == '*')
-      {
-        copy = "NAN";
-        break;
+      if (isCharExceptions(copy[i + 1]) || copy[i + 1] == '-');
+      else {
+        return "NAN";
+      }
+    }
+    else if (copy[i] == '!') {
+      if (i == 0) {
+        return "NAN";
       }
     }
   }
   return copy;
 }
-//Set current operation to be the previous one
-function setOperation(operations, index) {
-  var changedStrOperations = '';
-  if (index > 0) {
-    if (operations[index - 1] != 'C' && operations[index - 1] != 'P') {
-      changedStrOperations = operations.substring(0, index) + operations[index - 1] + operations.substring(index + 1);
-    }
-  }
-  return changedStrOperations;
+
+function verifyValidCharAfterFunction(equation, i, addIndex) {
+  if (equation[i + addIndex] == undefined)
+    return 0;
+  if (isdigit(equation[i + addIndex]) || equation[i + addIndex] == '-' || validateConstantFirstChar(equation[i + addIndex]))
+    return 1;
+  return 0;
 }
