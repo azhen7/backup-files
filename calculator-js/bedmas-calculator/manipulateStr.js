4b338da854e3ba526447da193f0711a7635383f0
azhen7 <75392766+azhen7@users.noreply.github.com>
2021-08-13 17:23:11 -0400

    Update manipulateStr.js

--- calculator-js/bedmas-calculator/manipulateStr.js
+++ calculator-js/bedmas-calculator/manipulateStr.js
@@ -1,49 +1,3 @@
-//Contains all the string-related functions
-function isCharExceptions(c) {
-  if (isdigit(c) || validateConstantChar(c) || c == ')' || c == ' ')
-    return 1;
-  return 0;
-}
-//Check whether c is indicative of function which requires two arguments (e.g. GCD, LCM, log, root, etc.)
-function isFunctionWithTwoArgs(c) {
-  if ((c.charCodeAt() >= '0'.charCodeAt() && c.charCodeAt() <= '3'.charCodeAt()) || (c >= 'A'.charCodeAt() && c <= 'B'.charCodeAt)) {
-    return 1;
-  }
-  return 0;
-}
-// Check whether c is an operation 
-function validateOperation(c) {
-  if (c == '+' || c == '*' || c == '-' || c == '/' || c == '^' || c == '%' || c == 'e' || c == 'C' || c == 'P' || c == ',' || c == 'x' ||
-      c == '!') {
-    return 0;
-  }
-  return 1;
-}
-// Check whether char is representative of a math constant (e.g. PI) 
-function validateConstantChar(c) {
-  if (c == 'Z' || c == 'T' || c == 'R' || c == 'G' || c == 'E' || c == 'Y')
-      return 1;
-  return 0;
-}
-// Check whether char is first character of a math constant (e.g. PI) 
-function validateConstantFirstChar(c) {
-  if (c == 'P' || c == 'G' || c == 'S' || c == 'R' || c == 'C' || c == 'p' || c == 's')
-      return 1;
-  return 0;
-}
-// Validate char
-function validNext(c) {
-  if (isdigit(c) || c == 'm' || c == ' ' || c == '-' || validateConstantChar(c) || validateOperation(c))
-      return 0;
-  return 1;
-}
-// Check if char is not valid
-function invalidChars(c) {
-  if (c == '<' || c == '>' || c == '?' || c == '@' || c == '#' || c == '$' || c == '&' || c == '{' || c == '}' || c == '[' || c == ']'
-     || c == '"' || c == ';' || c == ':' || c == '`' || c == '_' || c.charCodeAt() > 127)
-    return 1;
-  return 0;
-}
 // Replaces characters with spaces 
 function removeChar(input, index, c) {
   let changedStr = input.substring(0, index + 1);
@@ -70,7 +24,7 @@ function setUp(input) {
       }
       else if (isCharExceptions(copy[i + 1]));
       else {
-        return "NAN";
+        return null;
       }
     }
     else if (copy[i] == '-') {
@@ -82,17 +36,17 @@ function setUp(input) {
       }
       else if (isCharExceptions(copy[i + 1]));
       else {
-        return "NAN";
+        return null;
       }
     }
-    else if (copy[i] == '*' || copy[i] == 'x') {
-      copy = copy.substring(0, i) + '*' + copy.substring(i + 1);
+    else if (copy[i] == '*') {
+      //copy = copy.substring(0, i) + '*' + copy.substring(i + 1);
       if (i == 0) {
-        return "NAN";
+        return null;
       }
       if (i > 0) {
         if (copy[i - 1] == '/') {
-          return "NAN";
+          return null;
         }
       }
       if (copy[i + 1] == '*') {
@@ -100,21 +54,21 @@ function setUp(input) {
       }
       else if (isCharExceptions(copy[i + 1]) || copy[i + 1] == '-');
       else {
-        return "NAN";
+        return null;
       }
     }
     else if (copy[i] == '/') {
       if (i == 0) {
-        return "NAN";
+        return null;
       }
       if (isCharExceptions(copy[i + 1]) || copy[i + 1] == '-');
       else {
-        return "NAN";
+        return null;
       }
     }
     else if (copy[i] == '!') {
       if (i == 0) {
-        return "NAN";
+        return null;
       }
     }
   }
