eae41303511a0f051a7dd93080e976c5148cff86
adam zheng <adam.ray.zheng@gmail.com>
2021-08-24 23:42:09 -0400

    Update test file for array

--- stl/array/testArray.cpp
+++ stl/array/testArray.cpp
@@ -2,5 +2,9 @@
 #include "array.hpp"
 
 int main() {
-    
+    std_copy::array<int, 5> t;
+    for (int i = 1; i < 5; i++) {
+        t.push_back(i * i);
+    }
+    std_copy::array<int, 5>::iterator it;
 }
