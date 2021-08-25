403a38bf668abf1cfda4d262b795c4729d24fe09
adam zheng <adam.ray.zheng@gmail.com>
2021-08-25 12:48:13 -0400

    Update test file for vector

--- stl/vector/testVector.cpp
+++ stl/vector/testVector.cpp
@@ -2,10 +2,11 @@
 #include "vector.hpp"
 
 int main() {
-    std_copy::vector<int> t;
+    std_copy::vector<int> r;
     for (int i = 1; i < 10; i++) {
-        t.push_back(i * i);
+        r.push_back(i * i);
     }
+    std_copy::vector<int> t(r);
     std_copy::vector<int>::iterator it;
     for (it = t.begin(); it != t.end(); it++) {
         std::cout << *it << "\n";
