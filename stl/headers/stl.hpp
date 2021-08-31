ecbe6039098183eb108ecc016596188eae211338
adam zheng <adam.ray.zheng@gmail.com>
2021-08-31 15:22:30 -0400

    Removed at() and operator[] to let STL container implement it itself

--- stl/headers/stl.hpp
+++ stl/headers/stl.hpp
@@ -13,39 +13,6 @@ namespace std_copy {
             typedef std::size_t     size_type;
 
             pointer internalBuffer_;
-            size_type numberOfElements_; 
-        
-        public:
-            /**
-             * This function returns a reference to 
-             * the element at a specified index in 
-             * the vector.
-             * @param index The index of the element to retrieve.
-            */
-            reference at(size_type index) {
-                if (index >= numberOfElements_) {
-                    std::string err = "index (which is " + std::to_string(index) + ") >= this->size() (which is " + 
-                                    std::to_string(numberOfElements_) + ")";
-
-                    throw std::out_of_range(err);
-                }
-                return internalBuffer_[index];
-            }
-            /**
-             * The [] operator is overloaded to provide C-style array
-             * indexing.
-             * @param index The index of the element to retrieve.
-            */
-            reference operator[](long long index) {
-                if (index < 0) {
-                    index += numberOfElements_;
-                    if (index < 0) {
-                        std::string err = "absolute value of index (which is " + std::to_string(index * -1)
-                                          + ") > this->size() (which is " + std::to_string(numberOfElements_) + ")";
-                        throw std::out_of_range(err);
-                    }
-                }
-                return this->at(index);
-            }
+            size_type numberOfElements_;
     };
 }
