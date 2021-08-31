d413291747730fd86391c180dbb5957e3f46242c
adam zheng <adam.ray.zheng@gmail.com>
2021-08-31 15:23:48 -0400

    Added at() and operator[] functions

--- stl/array/array.hpp
+++ stl/array/array.hpp
@@ -163,6 +163,37 @@ namespace std_copy {
                 numberOfElements_--;
             }
             /**
+             * This function returns a reference to 
+             * the element at a specified index in 
+             * the vector.
+             * @param index The index of the element to retrieve.
+            */
+            reference at(size_type index) {
+                if (index >= numberOfElements_) {
+                    std::string err = "index (which is " + std::to_string(index) + ") >= this->size() (which is " + 
+                                    std::to_string(numberOfElements_) + ")";
+
+                    throw std::out_of_range(err);
+                }
+                return *(internalBuffer_ + index);
+            }
+            /**
+             * The [] operator is overloaded to provide C-style array
+             * indexing.
+             * @param index The index of the element to retrieve.
+            */
+            reference operator[](long long index) {
+                if (index < 0) {
+                    index += numberOfElements_;
+                    if (index < 0) {
+                        std::string err = "absolute value of index (which is " + std::to_string(index * -1)
+                                          + ") > this->size() (which is " + std::to_string(numberOfElements_) + ")";
+                        throw std::out_of_range(err);
+                    }
+                }
+                return this->at(index);
+            }
+            /**
              * This function returns a reference to the
              * first element in the array. If the vector
              * is empty, this function throws an exception.
