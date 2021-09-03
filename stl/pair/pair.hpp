8e194329236208ccdc5264ce5e9e9aa55917beac
adam zheng <adam.ray.zheng@gmail.com>
2021-09-03 15:10:35 -0400

    Moved pair struct into separate file

---
+++ stl/pair/pair.hpp
@@ -0,0 +1,92 @@
+namespace std_copy {
+    template <class T1, class T2>
+    struct pair {
+        T1  first;
+        T2  second;
+
+        pair(T1 f = T1(), T2 s = T2())
+            : first(f), 
+            second(s) 
+        {
+        }
+
+        pair (const pair& p)
+            : first(p.first),
+            second(p.second)
+        {
+        }
+
+        void swap(pair& p) {
+            T1 temp = first;
+            first = p.first;
+            p.first = temp;
+
+            T2 temp2 = second;
+            second = p.second;
+            p.second = temp2;
+        }
+        const pair& operator=(const pair& toAssign) {
+            first = toAssign.first;
+            second = toAssign.second;
+        }
+    };
+    template <class T1, class T2>
+    pair<T1, T2> make_pair(T1 f, T2 s) {
+        pair<T1, T2> p(f, s);
+        return p;
+    }
+
+    /**
+     * Check if two pair objects are equal.
+    */
+    template <class T1, class T2>
+    bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+        return (lhs.first == rhs.first) && (lhs.second == rhs.second);
+    }
+    /**
+     * Check if two pair objects are unequal.
+    */
+    template <class T1, class T2>
+    bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+        return !(lhs == rhs);
+    }
+    /**
+     * Check if one pair object is less than the other one.
+    */
+    template <class T1, class T2>
+    bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+        if (lhs.first < rhs.first)
+            return true;
+        else if (lhs.first < rhs.first)
+            return false;
+        else {
+            if (lhs.second < rhs.second)
+                return true;
+            else
+                return false;
+        }
+    }
+    /**
+     * Check if one pair object is less than or equal to the other pair 
+     * object.
+    */
+    template <class T1, class T2>
+    bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+        return !(rhs < lhs);
+    }
+    /**
+     * Check if one pair object is greater than the other pair object.
+    */
+    template <class T1, class T2>
+    bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+        return rhs < lhs;
+    }
+    /**
+     * Check if one pair object is greater than or equal to the other pair 
+     * object.
+    */
+    template <class T1, class T2>
+    bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+        return !(lhs < rhs);
+    }
+}
