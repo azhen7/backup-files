aecb19e67e222ba52616b71ec0597cd1f022c789
adam zheng <adam.ray.zheng@gmail.com>
2021-11-20 09:41:13 -0500

    <version>

---
+++ stl/version.hpp
@@ -0,0 +1,25 @@
+#ifndef _STD_COPY_VERSION
+#define _STD_COPY_VERSION
+
+#define std_copy_vector_implemented             202108L
+#define std_copy_array_implemented              202108L
+#define std_copy_iterator_funcs_implemented     202108L
+#define std_copy_map_implemented                202109L
+#define std_copy_pair_implemented               202109L
+#define std_copy_algorithm_implemented          202109L
+#define std_copy_move_implemented               202109L
+#define std_copy_type_traits_implemented        202109L
+#define std_copy_pointer_traits_implemented     202110L
+#define std_copy_allocator_implemented          202110L
+#define std_copy_iterator_traits_implemented    202110L
+#define std_copy_allocator_traits_implemented   202110L
+#define std_copy_char_traits_implemented        202111L
+#define std_copy_string_implemented             202111L
+#define std_copy_concept_implemented            202111L
+#define std_copy_functional_implemented         202111L
+#define std_copy_std_function_implemented       202111L
+#define std_copy_functional_comp_implemented    202111L
+#define std_copy_iterator_concepts_implemented  202111L
+#define std_copy_iterator_implemented           202111L
+
+#endif
