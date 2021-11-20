8cc2430727720a491f1816f4006883ef19958de3
adam zheng <adam.ray.zheng@gmail.com>
2021-11-20 09:31:05 -0500

    Put check-iterator concepts here (originally in algorithm.hpp)

--- stl/iterator_traits.hpp
+++ stl/iterator_traits.hpp
@@ -1,6 +1,51 @@
 #ifndef _STD_COPY_ITERATOR_TRAITS
 #define _STD_COPY_ITERATOR_TRAITS
 
+namespace _std_copy_hidden
+{
+    namespace _std_copy_iterator_traits
+    {
+        namespace _detail
+        {
+            template <class T, class U>
+            concept _is_same = std_copy::same_as<std_copy::remove_reference_t<T>, U>;
+            //Checks if Iterator is actually an iterator/pointer
+            template <class Iterator>
+            concept _is_iterator =
+            requires(Iterator it)
+            {
+                {*it} -> _is_same<typename std_copy::iterator_traits<Iterator>::value_type>;
+                {it++};
+                {++it};
+            };
+        }
+        
+        //Checks if InputIterator is an input iterator
+        template <class InputIterator>
+        concept _is_input_iterator = _detail::_is_iterator<InputIterator> &&
+        requires(InputIterator it, InputIterator it2)
+        {
+            {it == it2};
+            {it != it2};
+        };
+        //Checks if OutputIterator is an output iterator
+        template <class OutputIterator>
+        concept _is_output_iterator = _detail::_is_iterator<OutputIterator> &&
+        requires(OutputIterator it, typename std_copy::iterator_traits<OutputIterator>::value_type val)
+        {
+            {*it = val};
+        };
+        //Checks if BidirectionalIterator it a bidirectional iterator
+        template <class BidirectionalIterator>
+        concept _is_bidirectional_iterator = _is_input_iterator<BidirectionalIterator> && _is_output_iterator<BidirectionalIterator> &&
+        requires(BidirectionalIterator it)
+        {
+            {--it};
+            {it--};
+        };
+    }
+}
+
 namespace std_copy
 {
     struct input_iterator_tag {};
@@ -9,16 +54,20 @@ namespace std_copy
     struct bidirectional_iterator_tag : forward_iterator_tag {};
     struct random_access_iterator_tag : bidirectional_iterator_tag {};
 
-    template <class T>
+    template <class Iterator>
     struct iterator_traits
     {
-        typedef typename T::value_type              value_type;
-        typedef typename T::pointer                 pointer;
-        typedef typename T::reference               reference;
-        typedef typename T::difference_type         difference_type;
-        typedef typename T::iterator_category       iterator_category;
-    };
+        private:
+            template <class T>
+            using _pointer = typename T::pointer;
 
+        public:
+            typedef typename Iterator::value_type              value_type;
+            using pointer       =       _std_copy_hidden::_detector_t<void, _pointer, Iterator>;
+            typedef typename Iterator::reference               reference;
+            typedef typename Iterator::difference_type         difference_type;
+            typedef typename Iterator::iterator_category       iterator_category;
+    };
     /**
      * Partial specialization for pointer types
     */
@@ -30,6 +79,16 @@ namespace std_copy
         typedef T&                                      reference;
         typedef long long                               difference_type;
         typedef std_copy::random_access_iterator_tag    iterator_category;
+    };
+
+    template <class Category, class T, class Distance = long long, class Pointer = T*, class Reference = T&>
+    struct iterator
+    {
+        typedef Category            iterator_category;
+        typedef T                   value_type;
+        typedef Distance            difference_type;
+        typedef Pointer             pointer;
+        typedef Reference           reference;
     };
 }
 
