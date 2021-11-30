6c1280d173ce1c588ec658e59bab3286b12d052b
adam zheng <adam.ray.zheng@gmail.com>
2021-11-29 22:41:30 -0500

    Added more functions

--- stl/uninitialized_algo.hpp
+++ stl/uninitialized_algo.hpp
@@ -3,6 +3,8 @@
 
 #include "iterator_traits.hpp"
 #include "move.hpp"
+#include "concepts.hpp"
+#include "construct_destroy.hpp"
 
 namespace std_copy
 {
@@ -14,6 +16,8 @@ namespace std_copy
     */
     template <class InputIt, class ForwardIt>
     ForwardIt uninitialized_copy(InputIt first, InputIt last, ForwardIt result_first)
+        requires _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+                 && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<ForwardIt>
     {
         using T = typename iterator_traits<ForwardIt>::value_type;
         ForwardIt temp = result_first;
@@ -28,11 +32,7 @@ namespace std_copy
         }
         catch(...)
         {
-            while (temp != result_first)
-            {
-                temp->~T();
-                temp++;
-            }
+            destroy(temp, result_first);
             throw;
         }
         return result_first;
@@ -45,7 +45,8 @@ namespace std_copy
     */
     template <class InputIt, class Size, class ForwardIt>
     ForwardIt uninitialized_copy_n(InputIt first, Size n, ForwardIt result_first)
-        requires is_integral_v<Size>
+        requires integral<Size> && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+                 && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<ForwardIt>
     {
         using T = typename iterator_traits<ForwardIt>::value_type;
         ForwardIt temp = result_first;
@@ -61,11 +62,7 @@ namespace std_copy
         }
         catch(...)
         {
-            while (temp != result_first)
-            {
-                temp->~T();
-                temp++;
-            }
+            destroy(temp, result_first);
             throw;
         }
     }
@@ -77,6 +74,7 @@ namespace std_copy
     */
     template <class InputIt, class T>
     void uninitialized_fill(InputIt first, InputIt last, const T& val)
+        requires _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
     {
         using T = typename iterator_traits<InputIt>::value_type;
         InputIt temp = first;
@@ -90,11 +88,7 @@ namespace std_copy
         }
         catch(...)
         {
-            while (temp != first)
-            {
-                temp->~T();
-                temp++;
-            }
+            destroy(temp, first);
             throw;
         }
     }
@@ -106,7 +100,7 @@ namespace std_copy
     */
     template <class InputIt, class Size, class T>
     void uninitiialized_fill_n(InputIt first, Size n, const T& val)
-        requires is_integral_v<Size>
+        requires integral<Size> && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
     {
         using T = typename iterator_traits<InputIT>::value_type;
         InputIt temp = first;
@@ -121,12 +115,168 @@ namespace std_copy
         }
         catch(...)
         {
-            while (temp != first)
-            {
-                temp->~T();
-                temp++;
-            }
+            destroy(temp, first);
             throw;
+        }
+    }
+    /**
+     * Moves the range [first, last) to the uninitialized range starting at result_first.
+     * @param first An iterator to the start of the range to move.
+     * @param last An iterator to the end of the range to move.
+     * @param result_first An iterator to the start of the range to move to.
+    */
+    template <class InputIt, class ForwardIt>
+    ForwardIt uninitialized_move(InputIt first, InputIt last, ForwardIt result_first)
+        requires _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+                 && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<ForwardIt>
+    {
+        using T = typename iterator_traits<ForwardIt>::value_type;
+        ForwardIt temp = result_first;
+        try
+        {
+            while (first != last)
+            {
+                ::new (const_cast<void*>(static_cast<const volatile void*>(addressof(*result_first)))) T(move(*first));
+                first++;
+                result_first++;
+            }
+        }
+        catch(...)
+        {
+            destroy(temp, result_first);
+            throw;
+        }
+        return result_first;
+    }
+    /**
+     * Moves the first n elements starting from first to the uninitialized range starting at result_first.
+     * @param first An iterator to the start of the range to move.
+     * @param n The number of elements after first to move.
+     * @param result_first An iterator to the start of the range to move to.
+    */
+    template <class InputIt, class Size, class ForwardIt>
+    ForwardIt uninitialized_move_n(InputIt first, Size n, ForwardIt result_first)
+        requires integral<Size> && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+                 && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<ForwardIt>
+    {
+        using T = typename iterator_traits<ForwardIt>::value_type;
+        ForwardIt temp = result_first;
+        try
+        {
+            while (n > 0)
+            {
+                ::new (const_cast<void*>(static_cast<const volatile void*>(addressof(*result_first)))) T(move(*first));
+                first++;
+                result_first++;
+                n--;
+            }
+        }
+        catch(...)
+        {
+            destroy(temp, result_first);
+            throw;
+        }
+    }
+    /**
+     * Fills the uninitialized range [first, last) with constructed elements of T.
+     * @param first An iterator to the start of the uninitialized range.
+     * @param last An iterator to the end of the uninitialized range.
+    */
+    template <class InputIt, class T>
+    void uninitialized_default_constructed(InputIt first, InputIt last)
+        requires _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+    {
+        using T = typename iterator_traits<InputIt>::value_type;
+        InputIt temp = first;
+        try
+        {
+            while (first != last)
+            {
+                ::new (const_cast<void*>(static_cast<const volatile void*>(addressof(*first)))) T;
+                first++;
+            }
+        }
+        catch(...)
+        {
+            destroy(temp, first);
+            throw;
+        }
+    }
+    /**
+     * Fills the first n elements of the uninitialized range starting at first with constructed elements of T.
+     * @param first An iterator to the start of the uninitialized range.
+     * @param n The number of elements to assign.
+    */
+    template <class InputIt, class Size>
+    void uninitialized_default_constructed_n(InputIt first, Size n)
+        requires integral<Size> && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+    {
+        using T = typename iterator_traits<InputIT>::value_type;
+        InputIt temp = first;
+        try
+        {
+            while (n > 0)
+            {
+                ::new(const_cast<void*>(static_cast<const volatile void*>(addressof(*first)))) T;
+                first++;
+                n--;
+            }
+        }
+        catch(...)
+        {
+            destroy(temp, first);
+            throw;
+        }
+    }
+    /**
+     * Fills the uninitialized range [first, last) with default initialized values.
+     * @param first An iterator to the start of the uninitialized range.
+     * @param last An iterator to the end of the uninitialized range.
+    */
+    template <class InputIt, class T>
+    void uninitialized_value_construct(InputIt first, InputIt last)
+        requires _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+    {
+        using T = typename iterator_traits<InputIt>::value_type;
+        InputIt temp = first;
+        try
+        {
+            while (first != last)
+            {
+                ::new (const_cast<void*>(static_cast<const volatile void*>(addressof(*first)))) T();
+                first++;
+            }
+        }
+        catch(...)
+        {
+            destroy(temp, first);
+            throw;
+        }
+    }
+    /**
+     * Fills the first n elements of the uninitialized range starting at first with default initialized values.
+     * @param first An iterator to the start of the uninitialized range.
+     * @param n The number of elements to assign.
+    */
+    template <class InputIt, class Size, class aT>
+    void uninitialized_value_construct_n(InputIt first, Size n)
+        requires integral<Size> && _std_copy_hidden::_std_copy_iterator_traits::_is_input_iterator<InputIt>
+    {
+        using T = typename iterator_traits<InputIT>::value_type;
+        InputIt temp = first;
+        try
+        {
+            while (n > 0)
+            {
+                ::new(const_cast<void*>(static_cast<const volatile void*>(addressof(*first)))) T();
+                first++;
+                n--;
+            }
+        }
+        catch(...)
+        {
+            destroy(temp, first);
+            throw; 
         }
     }
 }
