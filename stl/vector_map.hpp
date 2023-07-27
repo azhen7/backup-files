7de514ce677b69a223ec6e79f1b599836217336f
Adam Zheng <adam.ray.zheng@gmail.com>
2023-07-27 12:38:31 -0400

    iterator typo

--- stl/vector_map.hpp
+++ stl/vector_map.hpp
@@ -43,10 +43,10 @@ namespace std_copy
             typedef Alloc                                       allocator_type;
             typedef std::size_t                                 size_type;
             typedef std::ptrdiff_t                              difference_type;
-            typedef _std_copy_hidden::_std_copy_stl_containers::_iterator<T*, _map_type> iterator;
-            typedef _std_copy_hidden::_std_copy_stl_containers::_iterator<const T*, _map_type> const_iterator;
-            typedef reverse_iterator<pointer>                   reverse_iterator;
-            typedef reverse_iterator<const_pointer>             const_reverse_iterator;
+            typedef _std_copy_hidden::_std_copy_stl_containers::_iterator<pointer, _map_type> iterator;
+            typedef _std_copy_hidden::_std_copy_stl_containers::_iterator<const_pointer, _map_type> const_iterator;
+            typedef std_copy::reverse_iterator<pointer>         reverse_iterator;
+            typedef std_copy::reverse_iterator<const_pointer>   const_reverse_iterator;
         
         protected:
             typedef pair<iterator, bool>                        _iterator_and_bool;
