b1b5382bf4edd58c60f633c15a2047dad08ed12b
Adam Zheng <adam.ray.zheng@gmail.com>
2023-07-27 14:40:51 -0400

    seed_seq

--- stl/random.hpp
+++ stl/random.hpp
@@ -3,15 +3,110 @@
 
 #include <cstdint>
 #include "type_traits.hpp"
+#include "vector.hpp"
+#include "iterator.hpp"
+#include "algorithm.hpp"
 
 namespace std_copy
 {
+    class seed_seq
+    {
+        public:
+            typedef std::uint_least32_t result_type;
+
+            seed_seq()
+            {
+            }
+
+            template <class InputIt>
+            seed_seq(InputIt first, InputIt last)
+            {
+                _internalBuffer.assign(first, last);
+            }
+
+            std::size_t size() { return _internalBuffer.size(); }
+            
+            template <class OutputIt>
+        #if __cplusplus > 201703L
+            requires output_iterator<OutputIt>
+        #endif
+            void param(OutputIt out)
+            {
+                std_copy::copy(_internalBuffer.begin(), _internalBuffer.end(), out);
+            }
+
+            template <class RandomIt>
+            void generate(RandomIt first, RandomIt last)
+        #if __cplusplus > 201703L
+            requires random_access_iterator<RandomIt>
+        #endif
+            {
+                std_copy::fill(first, last, 0x8b8b8b8b);
+
+                const std::size_t n = last - first;
+                const std::size_t s = this->size();
+                const std::size_t t = (n < 7) ? (n - 1) >> 1
+                                    : (n < 39) ? 3
+                                    : (n < 68) ? 5
+                                    : (n < 623) ? 7
+                                    : 11;
+                const std::size_t p = (n - t) >> 1;
+                const std::size_t q = p + t;
+                const std::size_t m = std_copy::max(s + 1, n);
+
+                {
+                    std::uint32_t r1 = 1371501266u;
+                    std::uint32_t r2 = r1 + s;
+                    first[p] += r1;
+                    first[q] += r2;
+                    first[0] = r2;
+                }
+
+                for (std::size_t k = 1; k < m; k++)
+                {
+                    std::size_t kp = (k + p) % n;
+                    std::size_t kq = (k + q) % n;
+                    std::size_t kmod = k % n;
+
+                    std::uint32_t res = first[kmod] ^ first[kp] ^ first[(k - 1) % n];
+                    std::uint32_t r1 = 1664525u * (res ^ (res >> 27));
+                    std::uint32_t r2 = (k <= s) ? r1 + kmod + _internalBuffer[k - 1]
+                                     :            r1 + kmod;
+                    first[kp] +=  r1;
+                    first[kq] += r2;
+                    first[kmod] = r2;
+                }
+
+                for (std::size_t k = m; k < m + n; k++)
+                {
+                    std::size_t kp = (k + p) % n;
+                    std::size_t kq = (k + q) % n;
+                    std::size_t kmod = k % n;
+
+                    std::uint32_t res = first[kmod] + first[kp] + first[(k - 1) % n];
+                    std::uint32_t r3 = 1566083941u * (res ^ (res >> 27));
+                    std::uint32_t r4 = r3 - kmod;
+                    first[kp] ^= r3;
+                    first[kq] ^= r4;
+                    first[kmod] = r4;
+                }
+            }
+
+        private:
+            vector<result_type> _internalBuffer;
+    };
+
     template <
         class UIntType,
         UIntType multiplierVal,
         UIntType incrementVal,
         UIntType modulusVal
     >
+    /**
+     * Given three variables a, b, c, calculates the next
+     * generated pseudo-random value with the formula
+     * _seed = ((_seed * a) + b) % c.
+    */
     class linear_congruential_engine
     {
         protected:
