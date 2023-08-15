ac0f93575e1784141028e92856e1c9a3d6865e92
Adam Zheng <adam.ray.zheng@gmail.com>
2023-08-14 22:33:13 -0400

    _node_iterator, _const_node_iterator

--- stl/forward_list.hpp
+++ stl/forward_list.hpp
@@ -4,6 +4,7 @@
 #include "allocator.hpp"
 #include "allocator_traits.hpp"
 #include <cstdint>
+#include "iterator.hpp"
 
 namespace _std_copy_hidden
 {
@@ -14,6 +15,7 @@ namespace _std_copy_hidden
         {
             private:
                 typedef _node<_T> _self_type;
+
             public:
                 _self_type* _next;
                 _T _value;
@@ -28,12 +30,116 @@ namespace _std_copy_hidden
         template <class T>
         struct _node_iterator
         {
+            private:
+                typedef _node<T> _node_type;
+                typedef _node_iterator<T> _self_type;
+                _node_type* _curr;
+            
+            public:
+                typedef std::ptrdiff_t difference_type;
+                typedef std_copy::forward_iterator_tag iterator_category;
+                typedef T value_type;
+                typedef T& reference;
+                typedef T* pointer;
 
+                _node_iterator(_node_type* c)
+                    : _curr(c)
+                {
+                }
+
+                _self_type operator++() noexcept
+                {
+                    _curr = _curr->_next;
+                    return *this;
+                }
+                _self_type operator++(int) noexcept
+                {
+                    _self_type temp = *this;
+                    _curr = _curr->_next;
+                    return temp;
+                }
+                T& operator*() const noexcept
+                {
+                    return _curr->_value;
+                }
+                _node_type* base() const noexcept
+                {
+                    return _curr;
+                }
+                void operator=(const _self_type& l) noexcept
+                {
+                    _curr = l._curr;
+                }
+                friend bool operator==(const _self_type& a, const _self_type& l)
+                {
+                    return a._curr == l._curr;
+                }
+                friend bool operator!=(const _self_type& a, const _self_type& l)
+                {
+                    return !(a == l);
+                }
         };
 
         template <class T>
         struct _const_node_iterator
         {
+            private:
+                typedef const _node<T> _node_type;
+                typedef _const_node_iterator<T> _self_type;
+                const _node_type* _curr;
+
+            public:
+                typedef std::ptrdiff_t difference_type;
+                typedef std_copy::forward_iterator_tag iterator_category;
+                typedef T value_type;
+                typedef const T& reference;
+                typedef const T* pointer;
+
+                _const_node_iterator(const _node_type *e)
+                    : _curr(e)
+                {
+                }
+
+                _const_node_iterator() : _curr() {}
+
+                template <class _T>
+                _const_node_iterator(const _node_iterator<_T>& n)
+                    : _curr(n.base())
+                {
+                }
+
+                _self_type operator++() noexcept
+                {
+                    _curr = _curr->_next;
+                    return *this;
+                }
+                _self_type operator++(int) noexcept
+                {
+                    _self_type temp = *this;
+                    _curr = _curr->_next;
+                    return temp;
+                }
+        
+                T& operator*() const noexcept
+                {
+                    return _curr->_value;
+                }
+                _node_type* base() const noexcept
+                {
+                    return _curr;
+                }
+                void operator=(const _node_iterator<T> &l) noexcept
+                {
+                    _curr = l._curr;
+                }
+                friend bool operator==(const _self_type& a, const _self_type& l)
+                {
+                    return a._curr == l._curr;
+                }
+                friend bool operator!=(const _self_type& a, const _self_type& l)
+                {
+                    return !(a == l);
+                }
         };
     }
 };
